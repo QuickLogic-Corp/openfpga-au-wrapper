5022e4b36ea9da69836cf02c0e5332c7b070b928
coolbreeze413 <coolbreeze413@yahoo.co.in>
2025-04-24 16:14:41 +0530

    bison to bash_profile

--- .github/workflows/install_dependencies_build_macos.sh
+++ .github/workflows/install_dependencies_build_macos.sh
@@ -1,8 +1,9 @@
 bison -V
 brew unlink bison
 brew install bison
-echo 'export PATH="/usr/local/opt/bison/bin:$PATH"' >> ~/.bashrc
+echo 'export PATH="/usr/local/opt/bison/bin:$PATH"' >> /Users/runner/.bash_profile
 export PATH=/usr/local/opt/bison/bin:$PATH
+echo 'export LDFLAGS="-L/usr/local/opt/bison/lib"' >> /Users/runner/.bash_profile
 export LDFLAGS="-L/usr/local/opt/bison/lib"
-source ~/.bashrc
+source ~/.bash_profile
 bison -V
